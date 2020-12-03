923bebcbae1dbc4a80eb36f22717c5b46b9ed928
pedrogarciyalopez <macmacins@inbox.ru>
2020-12-03 13:51:30 +0300

    test4

--- build/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './components/Yamap';
-export * from './components/Marker';
-export * from './components/Polygon';
-export * from './components/Polyline';
-export * from './components/Circle';
-export * from './geocoding';
-export * from './interfaces';
-export { YaMap as default } from './components/Yamap';
